ac4963288065e1492696d0bf09379a2d8516a3f5
Hayden <linhaijin@gmail.com>
2026-08-11 22:47:41 +1000

    Increase upload server action limit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverActions: {},
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
   },
 };
 
